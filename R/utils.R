9ce4358212035a7f0b7fb61d63f4c5b925606af1
Michael Lawrence <lawremi@gmail.com>
2011-09-10 16:49:10 -0700

    Rename lazyField to fieldWithPrototype. Probably a more descriptive name. Add prototype (default value) support to signalingFields().

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,7 @@
 ## utils
 ## ======================================================================
 
+### FIXME: do we really want to keep signalingField() around?
 
 ##' Convenience function for defining a reference class field that
 ##' signals when set.
@@ -43,10 +44,11 @@ signalingField <- function(name, class,
 ##' Convenience function for defining a set of reference class fields that
 ##' signals when set.
 ##'
-##' When constructing signaling fields in this way, each field has the ability
-##' to register its own signal and at the same time, there is one top level signal
-##' which could be emitted no matter which field changes. Please see the example
-##' to learn to register global signal and individual signal.
+##' When constructing signaling fields in this way, each field has the
+##' ability to register its own signal and at the same time, there is
+##' one top level signal which could be emitted no matter which field
+##' changes. Please see the example to learn to register global signal
+##' and individual signal.
 ##'
 ##' @title Signaling Fields 
 ##' @param fields list of names of the field and associated fields class 
@@ -55,23 +57,37 @@ signalingField <- function(name, class,
 ##' @author Michael Lawrence, Tengfei Yin
 ##' @example objectSignals/inst/examples/signalingFields.R
 ##' @export
-signalingFields <- function(fields, signalName = "changed") {
+signalingFields <- function(fields, prototype = list(), signalName = "changed")
+{
   if (!length(fields))
     return(list())
   .fieldNames <- paste(".", names(fields), sep = "")
-  activeFields <- mapply(function(fieldClass, fieldName, .fieldName,
-                                  thisSignal) {
+  .initNames <- paste(".init.", names(fields), sep = "")
+  hasPrototype <- names(fields) %in% names(prototype)
+  if (any(sapply(fields, is.function) & hasPrototype))
+    stop("An active binding field cannot have a prototype")
+  activeFields <- mapply(function(fieldClass, fieldName, .fieldName, initName,
+                                  hasPrototype, prototype, thisSignal)
+  {
     as.function(c(alist(val=), substitute({
       if (missing(val)) {
+        if (hasPrototype && !length(initName)) {
+          .fieldName <<- prototype
+          initName <<- TRUE
+        }
         .fieldName
-      } else { ### FIXME: should share code with signalingField, above
-        if (!is(val, fieldClass))
-          stop("Cannot set an object of type '", class(val), "' on '",
-               fieldName, "', a field of type '", fieldClass, "'")
-        else val <- as(val, fieldClass, strict = FALSE)
-        fieldHasChanged <- !identical(.fieldName, val)
+      } else {
+        if (!is.function(fieldClass)) {
+          if (!is(val, fieldClass))
+            stop("Cannot set an object of type '", class(val), "' on '",
+                 fieldName, "', a field of type '", fieldClass, "'")
+          else val <- as(val, fieldClass, strict = FALSE)
+        }
+        ## careful here; if field is active binding, it might not change
+        oldVal <- .fieldName
         .fieldName <<- val
-        if (fieldHasChanged) {
+        if (hasPrototype) initName <<- TRUE
+        if (!identical(oldVal, .fieldName)) {
           signalName$emit(fieldName)
           thisSignal$emit()
         }
@@ -79,36 +95,45 @@ signalingFields <- function(fields, signalName = "changed") {
     }, list(.fieldName = as.name(.fieldName),
             fieldClass = fieldClass, fieldName = fieldName,
             thisSignal = as.name(thisSignal),
+            initName = as.name(initName),
+            hasPrototype = hasPrototype,
+            prototype = prototype,
             signalName = as.name(signalName)))))
-  }, fields, names(fields), .fieldNames, paste(names(fields), "Changed", sep = ""))
-  indSigs <- lapply(names(fields), function(nm){
-      nm <- paste(nm, "Changed", sep = "")
-      lazyField(nm,"Signal", Signal())
-    })
+  }, fields, names(fields), .fieldNames, .initNames, hasPrototype,
+     prototype[names(fields)], paste(names(fields), "Changed", sep = ""))
+  indSigs <- lapply(names(fields), function(nm) {
+    nm <- paste(nm, "Changed", sep = "")
+    fieldWithPrototype(nm, "Signal", Signal())
+  })
   c(activeFields, structure(fields, names = .fieldNames),
-    lazyField(signalName, "Signal", Signal(name)),
+    structure(rep("logical", sum(hasPrototype)),
+              names = .initNames[hasPrototype]),
+    fieldWithPrototype(signalName, "Signal", Signal(name)),
     unlist(indSigs))
 }
 
 
-##' Declares a lazily initialized field, with the class and initializer.
+##' A convenience for declaring a default value for a field, in the
+##' vein of \code{\link[methods]{prototype}} for S4 classes, except
+##' the default value is quoted and evaluated lazily.
 ##'
-##' @title Lazily initialize the fileds
+##' @title Fields with prototypes
 ##' @param name The name of the field
 ##' @param class The class of the field
-##' @param expr Expression that when evaluated initializes the field
+##' @param value Default value that when evaluated
+##' initializes the field
 ##' @return A list suitable for use with \code{\link{setRefClass}}
 ##' @author Michael lawrence
 ##' @export
 
-lazyField <- function(name, class, expr) {
+fieldWithPrototype <- function(name, class, value) {
   .name <- paste(".", name, sep = "")
   .init <- paste(".init", name, sep = ".")
-  expr <- substitute(expr)
+  value <- substitute(value)
   body <- substitute({
     if (missing(val)) {
       if (!length(.init)) {
-        .name <<- expr
+        .name <<- value
         .init <<- TRUE
       }
       .name
@@ -120,7 +145,7 @@ lazyField <- function(name, class, expr) {
       .name <<- val
       .init <<- TRUE
     }
-  }, list(.name = as.name(.name), name = name, .class = class, expr = expr,
+  }, list(.name = as.name(.name), name = name, .class = class, value = value,
           .init = as.name(.init)))
   structure(list(as.function(c(alist(val=), body)), class, "logical"),
             names = c(name, .name, .init))
@@ -144,7 +169,7 @@ declareSignal <- function(expr) {
   expr <- substitute(expr)
   name <- deparse(expr[[1]])
   expr[[1]] <- quote(Signal)
-  do.call(lazyField, list(name, "Signal", expr))
+  do.call(fieldWithPrototype, list(name, "Signal", expr))
 }
 
 
