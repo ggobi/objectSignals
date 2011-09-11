1208d365568ddcf05ee2f9d842e84ad81095b7c5
tengfei <tengfei@tengfei-T510.(none)>
2011-09-11 00:05:32 -0500

    remove unused examples

--- inst/examples/signalingField.R
+++
@@ -1,5 +0,0 @@
-Brush.gen <- setRefClass("Brush",
-fields = signalingField("color", "character"))
-brush <- Brush.gen$new(color = "blue")
-brush$colorChanged$connect(function() print(brush$color))
-brush$color <- "red"
--- inst/examples/signalingFields.R
+++
@@ -1,9 +0,0 @@
-Brush.gen <- setRefClass("Brush",
-                       fields = signalingFields(list(color = "character",
-                          size = "numeric"), signalName = "brushChanged"))
-brush <- Brush.gen$new(color = "red", size = 2)
-brush$brushChanged$connect(function(x){print(paste(x,
-"changed, Global signal emited"))})
-brush$colorChanged$connect(function() print("color-changed individual signal emited"))
-brush$size <- 3
-brush$color <- "blue"
